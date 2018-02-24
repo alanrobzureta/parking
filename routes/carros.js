49c4ca68576200abec85b06d86eef7cfee3b332d
Familia Gomes <Familia Gomes@192.168.1.3>
2018-02-23 22:59:44 -0300

    Projeto Final - Integrando tudo

--- routes/carros.js
+++ routes/carros.js
@@ -3,8 +3,14 @@ var router = express.Router();
 var service = require('../services/carros');
 
 router.get('/',service.find);
+
 router.get('/new',service.new);
+router.post('/',service.create);
+
 router.get('/edit/:id',service.edit);
+router.put('/:id',service.update);
+
+router.delete('/:id',service.remove);
 
 
 module.exports = router;
--- services/carros.js
+++ services/carros.js
@@ -1,6 +1,16 @@
+var Carros = require('./../models/carros');
+
 // Action find
 exports.find = function(req, res){
-    res.render('lista_carros');
+    Carros.find({},function(err, carros){
+        if(err){
+            return;
+        }
+        
+        res.render('lista_carros',{
+            carros : carros
+        });
+    })    
 }
 
 //action new
@@ -8,7 +18,48 @@ exports.new = function(req, res){
     res.render('novo_carro');
 }
 
+//action create
+exports.create = function(req, res){
+    Carros.create(req.body,function(err, carros){
+        if(err){
+            return;
+        }
+        res.redirect('/carros');
+    });
+}
+
 //action new
 exports.edit = function(req, res){
-    res.render('atualizar_carro');
+    Carros.findById(req.params.id,function(err,carros){
+        if(err){
+            return;
+        }
+        res.render('atualizar_carro',{
+           carros: carros 
+        });
+    });
+}
+
+//action update
+exports.update = function(req, res){
+    Carros.update({
+        _id: req.params.id        
+    }, req.body, function(err, carros){
+        if(err){
+            return; 
+        }
+        res.redirect('/carros');
+    });
+}
+
+// action remove
+exports.remove = function(req, res){
+    Carros.remove({
+        _id: req.params.id
+    },function(err){
+        if(err){
+            return;
+        }
+        res.redirect('/carros');
+    });
 }
